aeef398ddf7412b6cd6e62c8c8a3808acaf18e1f
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-05-05 20:48:00 -0400

    added question 1

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 var start = document.querySelector('.start');
 var time = document.querySelector('#timer');
 var radioDisplay = document.querySelector('.input-box');
+var next = document.querySelector('#next');
+
+// var questions = [question1(), question2(), question3(), question4()];
+// var correctAns = []
 
 
 start.addEventListener('click', function () {
 setTime();
-radioDisplay.setAttribute("style", "display: flex");
-})
+radioDisplay.setAttribute("style", "display: block");
 
+});
 
 //sets a timer of 120s, and stops when it reaches 0s
 function setTime() {
     var timeLeft = 120;
-    time.textContent = timeLeft;
+    time.textContent = timeLeft;``
 
     var timeInterval = setInterval(function () {
         timeLeft--;
@@ -23,4 +27,22 @@ function setTime() {
         }
     }, 1000);
 }
+
+next.addEventListener('click', function(event){
+    event.preventDefault();
+})
+
+var questionObj = {
+    question1: question1(),
+
+
+}
+
+function question1(){
+    //stores the value of the checked radio button to variable
+    var selectedAns = document.querySelector('input[name="a"]:checked').value;
+    return selectedAns;
+}
+
+
 
