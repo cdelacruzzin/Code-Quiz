6306e571d7c8f8c3d4720a50193499537c9d223d
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-05-05 23:08:01 -0400

    'next' button successfully navigates through questions

--- script.js
+++ script.js
@@ -7,16 +7,14 @@ var radios = document.querySelectorAll('input');
 var labels = document.querySelectorAll('label');
 
 //a counter to keep track of which question we are on
-var questionNum = 0;
+var questionNum = 1;
 var questionArray = [question1, question2, question3, question4];
 
 start.addEventListener('click', function () {
 setTime();
 radioDisplay.setAttribute("style", "display: block");
 
-navigate(questionNum);
-
-
+question1();
 
 });
 
